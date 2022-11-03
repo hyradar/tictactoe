aec316ed928ea6a6885a43a71b9ed2ddc8a387b4
Hunter Yeago <hyradar@tutamail.com>
2022-11-03 16:07:44 +1100

    playing board is now javascript generated, created skeleton for gameboard array and player factory function

--- javascript.js
+++ javascript.js
@@ -1,14 +1,43 @@
 
-//One of: Module
-//Multiple of (players): Function Factories
 
-//Display Board Function
+//Render Board Function
+renderGameboard();
 
-let gamegrid = document.querySelector('.gamegrid');
+//Creates gameboard array from DOM cells
+let gameboard = document.querySelectorAll('.cell');
+gameboard = Array.from(gameboard);
 
-//creates cells NodeList / adds click eventlistener
-let cells = document.querySelectorAll('.cell').forEach(item => {
-    item.addEventListener('click', function() {
-        console.log('hello world');
+console.log({gameboard});
+
+//Adds click event listener
+gameboard.forEach(item => {
+    item.addEventListener('click', function() {    
+    console.log('you clicked a square');
     });
 });
+
+
+
+//Object to control the flow of the game - Use a module
+
+//Player Factory Function
+const Player = (name) => {
+    
+    let isTurn = false;
+    // const makeChoice 
+
+}
+
+//Create and Append Elements 
+function renderGameboard () {
+    let grid = document.createElement('div');
+    let body = document.querySelector('.body');
+    grid.className = 'gamegrid';
+    body.append(grid);
+
+    for (let i = 0; i < 9; i++) {
+        let newCell = document.createElement('div');
+        newCell.className = 'cell';
+        grid.append(newCell);
+    }
+}
